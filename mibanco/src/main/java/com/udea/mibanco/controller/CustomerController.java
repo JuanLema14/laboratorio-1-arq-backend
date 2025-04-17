16dd12ab0fc09d15a5c27c7a0f8bd5cd18a95a59
JuanLema14 <andres.lema1@udea.edu.co>
2025-04-17 11:44:28 -0500

    Crear servicio para permitir búsquedas filtradas en el listado de clientes

--- mibanco/src/main/java/com/udea/mibanco/controller/CustomerController.java
+++ mibanco/src/main/java/com/udea/mibanco/controller/CustomerController.java
@@ -2,39 +2,49 @@ package com.udea.mibanco.controller;
 
 import com.udea.mibanco.DTO.CustomerDTO;
 import com.udea.mibanco.service.CustomerService;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
 
-    private final CustomerService customerFacade;
-    public CustomerController(CustomerService customerService){
-        this.customerFacade = customerService;
+  private final CustomerService customerFacade;
+
+  public CustomerController(CustomerService customerFacade) {
+    this.customerFacade = customerFacade;
+  }
+
+  @GetMapping("/listar")
+  public ResponseEntity<List<CustomerDTO>> getAllCustomers(
+    @RequestParam(required = false) String firstName,
+    @RequestParam(required = false) String lastName,
+    @RequestParam(required = false) Double balanceMin,
+    @RequestParam(required = false) Double balanceMax
+  ) {
+    List<CustomerDTO> customers = customerFacade.getAllCustomersWithFilters(
+      firstName,
+      lastName,
+      balanceMin,
+      balanceMax
+    );
+    return ResponseEntity.ok(customers);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<CustomerDTO> getCustomerById(@PathVariable Long id) {
+    return ResponseEntity.ok(customerFacade.getCustomerById(id));
+  }
+
+  @PostMapping("/crear")
+  public ResponseEntity<CustomerDTO> createCustomer(
+    @RequestBody CustomerDTO customerDTO
+  ) {
+    if (customerDTO.getBalance() == null) {
+      throw new IllegalArgumentException("Balance cannot be null");
     }
 
-    //Obtener todos los clientes
-    @GetMapping
-    public ResponseEntity<List<CustomerDTO>> getAllCustomers(){
-        return ResponseEntity.ok(customerFacade.getAllCustomers());
-    }
-
-    //Obtener un cliente por un id
-    @GetMapping("/{id}")
-    public ResponseEntity<CustomerDTO> getCustomerById(@PathVariable Long id){
-        return  ResponseEntity.ok(customerFacade.getCustomerById(id));
-    }
-
-    //Crear un nuevo Cliente
-    @PostMapping
-    public ResponseEntity<CustomerDTO> createCustomer(@RequestBody CustomerDTO customerDTO){
-        if(customerDTO.getBalance() == null){
-            throw new IllegalArgumentException("Balance cannot be null");
-        }
-
-        return ResponseEntity.ok(customerFacade.createCustomer(customerDTO));
-    }
+    return ResponseEntity.ok(customerFacade.createCustomer(customerDTO));
+  }
 }
